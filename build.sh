31649cb06317281646adcd2cb8916e6853c11082
nidhi__bhat <nidhimbhat.21@gmail.com>
2026-01-16 17:59:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit
 # 1. Force install build tools and six globally first
 pip install --upgrade pip setuptools wheel six
 
-# 2. Install the rest of the requirements
-pip install -r requirements.txt
+# 2. Install the requirements BUT disable "build isolation"
+# This flag tells pip: "Don't ignore the 'six' I just installed above."
+pip install --no-build-isolation -r requirements.txt
